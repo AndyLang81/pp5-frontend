24e9af6814b50def83f85671ec2e0b4f91a0d9db
AndyLang81 <Andylanghoff@gmail.com>
2025-05-25 00:06:16 +0200

    Pass API_URL to child components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import TaskList from './TaskList';
-import './App.css'; // import the styling
+import './App.css';
 
 function App() {
   const [token, setToken] = useState(null);
+  const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
   const handleLogin = (newToken) => {
     setToken(newToken);
@@ -25,9 +26,9 @@ function App() {
       <h1>PP5 Task Manager</h1>
 
       {!token ? (
-        <Login onLogin={handleLogin} />
+        <Login onLogin={handleLogin} API_URL={API_URL} />
       ) : (
-        <TaskList token={token} />
+        <TaskList token={token} API_URL={API_URL} />
       )}
     </div>
   );
